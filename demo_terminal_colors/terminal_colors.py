e095a47d036619ec8265cb03644fc1da3528cc02
Breno Ribeiro do Valle <brenordv@users.noreply.github.com>
2019-09-16 22:55:21 -0300

    Adding new snippet (terminal colors)

---
+++ demo_terminal_colors/terminal_colors.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+	print('\033[0;0m**Black (30) \n\033[1;30mForeground text\n\033[1;40mBackground text')
+	print('\033[0;0m**Red (31) \n\033[1;31mForeground text\n\033[1;41mBackground text')
+	print('\033[0;0m**Green (32) \n\033[1;32mForeground text\n\033[1;42mBackground text')
+	print('\033[0;0m**Yellow (33) \n\033[1;33mForeground text\n\033[1;43mBackground text')
+	print('\033[0;0m**Blue (34) \n\033[1;34mForeground text\n\033[1;44mBackground text')
+	print('\033[0;0m**Magenta (35) \n\033[1;35mForeground text\n\033[1;45mBackground text')
+	print('\033[0;0m**Cyan (36) \n\033[1;36mForeground text\n\033[1;46mBackground text')
+	print('\033[0;0m**Light Gray (37) \n\033[1;37mForeground text\n\033[1;47mBackground text')
+	print('\033[0;0m**Dark Gray (90) \n\033[1;90mForeground text\n\033[1;100mBackground text')
+	print('\033[0;0m**Light Red (91) \n\033[1;91mForeground text\n\033[1;101mBackground text')
+	print('\033[0;0m**Light Green (92) \n\033[1;92mForeground text\n\033[1;102mBackground text')
+	print('\033[0;0m**Light Yellow (93) \n\033[1;93mForeground text\n\033[1;103mBackground text')
+	print('\033[0;0m**Light Blue (94) \n\033[1;94mForeground text\n\033[1;104mBackground text')
+	print('\033[0;0m**Light Magenta (95) \n\033[1;95mForeground text\n\033[1;105mBackground text')
+	print('\033[0;0m**Light Cyan (96) \n\033[1;96mForeground text\n\033[1;106mBackground text')
+	print('\033[0;0m**White (97) \n\033[1;97mForeground text\n\033[1;107mBackground text')
+
+
+
+if __name__ == '__main__':
+	main()
